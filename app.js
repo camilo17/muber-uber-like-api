41be4dc6c4ea11f5ef1fe95e4b75f46d8ef532f9
camilo17 <cami172013@gmail.com>
2017-05-19 12:34:30 -0500

    created a test folder and added supertest.

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const app = express(); 
 
+app.get('/api', (req, res) => {
+    res.send({ hi: 'there'}); 
+    console.log(req);
+    
+})
+
+
 module.exports = app; 
