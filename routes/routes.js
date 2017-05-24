c23f3fc8c950a71b6e6f7654d6d30bd8397ec9e3
camilo17 <cami172013@gmail.com>
2017-05-23 20:02:02 -0500

    Created drivers_controller file.

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const DriversController = require('../controllers/drivers_controller');
 module.exports = (app) => {
 
     app.get('/api', DriversController.greeting)
-    
-}); 
 
-};
+    app.post('/api/drivers', DriversController.create); 
+    
+}; 
+
