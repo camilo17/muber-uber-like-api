d3efbecd0249cef3e3abf5495d823a8d614d3971
camilo17 <cami172013@gmail.com>
2017-05-23 20:13:50 -0500

    created drivers_controller_test to test out the drivers controller.

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,6 +1,13 @@
+
+
 module.exports = {
     greeting(req, res) {
         res.send({hi: 'there'})
+    },
+    create(req, res) {
+        console.log(req.body); 
+        res.send({hi: 'there'}); 
+
     }
 
 };
